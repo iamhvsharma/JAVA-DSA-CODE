ea5079fd60158a6fc26029ad62c13fec82363cba
iamhvsharma <itshvsharma@gmail.com>
2023-09-25 23:06:23 +0530

    Day2

---
+++ input.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class input{
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        // sc.next function takes input of only one word!
+        // String input = sc.next();
+        // System.out.println(input);
+
+        
+         // sc.nextLine function takes input of complete line or paragraph
+        String name = sc.nextLine();
+        System.out.println(name);
+
+        // sc.nextInt function is used to take input of Integers
+        int number = sc.nextInt();
+        System.out.println(number);
+
+
+        //  
+
+       
+
+
+    }
+}
---
+++ sum.java
@@ -0,0 +1,10 @@
+public class sum{
+    public static void main(String args[]){
+        // System.out.println("Hellow Bro");
+        int a = 12;
+        int b = 32;
+        int sum = a + b;
+        System.out.println(sum);
+
+    }
+}
