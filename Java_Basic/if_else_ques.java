ae8b3ee658da27f460758ad04de615856233bf7e
iamhvsharma <itshvsharma@gmail.com>
2023-10-04 22:40:51 +0530

    Conditonal

---
+++ Java_Basic/if_else_ques.java
@@ -0,0 +1,14 @@
+public class if_else_ques{
+    public static void main(String args[]){
+        int A = 1;
+        int B = 3;
+
+        if(A>B){
+            System.out.println("A is Greater than B");
+        } else{
+            System.out.println("B is greater than A");
+        }
+
+
+    }
+}
