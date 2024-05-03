bf34cfbc9458242a10fa606d62261fbfa22b42f3
iamhvsharma <itshvsharma@gmail.com>
2024-05-03 14:51:32 +0530

    Switch

---
+++ Java_Basic/SwitchStatement.java
@@ -0,0 +1,15 @@
+public class SwitchStatement{
+    public static void main(String args[]){
+        int name = 1;
+        switch(name) {
+            case 1: System.out.println("Harsh");
+            break;
+            case 2: System.out.println("Sam");
+            break;
+            case 3: System.out.println("Don");
+            break;
+            default: System.out.println("No name");
+            break;
+        }
+    }
+}
