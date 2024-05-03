1d1a1f37844afba33b795fdbe2dca74eb4646edc
iamhvsharma <itshvsharma@gmail.com>
2024-05-04 00:49:24 +0530

    Loops

---
+++ Java_Basic/Loops.java
@@ -0,0 +1,36 @@
+public class Loops{
+    public static void main(String args[]){
+        // while loop 
+
+        // Syntax
+        // while(condition){
+        //     //do something
+        // }
+
+    //     int i = 0;
+    //     while(i <= 10){
+    //         System.out.println(i);
+    //         i++;
+    //     }
+
+
+        // for loop 
+
+        // for(int i = 1; i<=10; i++){
+        //     System.out.println("Hello Baby");
+        // }
+
+
+        // do while loop
+        // Syntax
+        // do{
+        //     //do Something
+        // } while(condition);
+
+
+        // Break Statement - to exit the loop 
+        // Continue Statement - 
+
+
+    }
+}
