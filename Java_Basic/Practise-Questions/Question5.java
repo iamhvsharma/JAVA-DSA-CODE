810e1e7a1ca19a855b16c6726c4587c357c7ac62
iamhvsharma <itshvsharma@gmail.com>
2024-05-03 23:06:36 +0530

    Fever check

---
+++ Java_Basic/Practise-Questions/Question5.java
@@ -0,0 +1,11 @@
+public class Question5{
+    public static void main(String args[]){
+        double temp = 103.5;
+        if(temp > 100){
+            System.out.println("You have a Fever");
+        } else{
+            System.out.println("You don't have a Fever");
+
+        }
+    }
+}
