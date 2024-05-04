a7ddf6a5246ced40997f70d3972c429d396caa54
iamhvsharma <itshvsharma@gmail.com>
2024-05-04 23:37:06 +0530

    Question wrote

--- Patterns/Question1.java
+++ Patterns/Question1.java
@@ -1,3 +1,10 @@
+// Question: Print the below pattern
+
+// *
+// **
+// ***
+// ****
+
 public class Question1{
     public static void main(String args[]){
         for(int line = 1; line<=4; line++){
