b30533f85395255e800ef53ef07a33918d43aa12
iamhvsharma <itshvsharma@gmail.com>
2024-05-05 19:46:14 +0530

    Char patterns

--- Patterns/Question2.java
+++ Patterns/Question2.java
@@ -9,8 +9,8 @@
 
  class Question2{
     public static void main(String args[]){
-        int n = 4;
-        for(int line = 1; line<=4; line++){
+        int n = 4; // Loop Controller
+        for(int line = 1; line<=n; line++){
             for(int stars = 1; stars <= n-line+1; stars++){
                 System.out.print("*");
             }
