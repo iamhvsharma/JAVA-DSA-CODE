f8c6e4c2095ae8458afe7e83de34a7f960b50a5a
iamhvsharma <itshvsharma@gmail.com>
2024-05-05 20:31:50 +0530

    Functions and Methods

---
+++ Functions/Basics.java
@@ -0,0 +1,24 @@
+public class Basics{
+
+    // Making first Function
+    public static void printMyName(){
+        System.out.println("Harshvardhan");
+    }
+
+
+    public static void main(String args[]){
+        // Funtions/ Methods are the reusable block of code which is used to perform a certain task.
+
+        // Syntax
+
+        // returnType name(){
+        //     // body
+        //     return statement;
+        // }
+
+        // Function Call
+        printMyName();
+
+
+    }
+}
