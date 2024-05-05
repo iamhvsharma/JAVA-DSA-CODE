e54ca2d01fda9f798a27293d36c5f822131161f3
iamhvsharma <itshvsharma@gmail.com>
2024-05-05 20:50:23 +0530

    Sum of a and b using functions

---
+++ Functions/SumOfAB.java
@@ -0,0 +1,18 @@
+// Sum of a and b using functions
+
+import java.util.*;
+
+public class SumOfAB{
+    public static void calculateSum(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a and b: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b;
+        System.out.println("Sum is : " + sum);
+    }
+
+    public static void main(String args[]){
+        calculateSum();
+    }
+}
