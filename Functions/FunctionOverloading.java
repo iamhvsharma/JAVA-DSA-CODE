668b2ba0598f95b0cb6e051f335c240b9d534444
iamhvsharma <itshvsharma@gmail.com>
2024-05-07 00:15:57 +0530

    Function Overloading using datatypes

--- Functions/FunctionOverloading.java
+++ Functions/FunctionOverloading.java
@@ -1,15 +1,26 @@
 public class FunctionOverloading{
+
+    // Function Overloading using Parameters
+    // public static int sum(int a, int b){
+    //     return a + b;
+    // }
+
+    // public static int sum(int a, int b, int c){
+    //     return a + b + c;
+    // }
+
+    // Function Overloading using Datatypes
     public static int sum(int a, int b){
         return a + b;
     }
 
-    public static int sum(int a, int b, int c){
-        return a + b + c;
+    public static float sum(float a, float b){
+        return a + b;
     }
 
 
     public static void main(String args[]){
         System.out.println(sum(10, 16));
-        System.out.println(sum(10, 12, 24));
+        System.out.println(sum(10.5f, 11.2f));
     }
 }
