1cf8fbbe4461ebf6e1db48017bd78992060b2087
iamhvsharma <itshvsharma@gmail.com>
2024-05-07 14:41:49 +0530

    Array

---
+++ Arrays/Array.java
@@ -0,0 +1,10 @@
+public class Array{
+    public static void main(String args[]){
+        int marks[] = new int[50];
+        // Array are static 
+        
+        int numbers[] = {1, 2, 3};
+
+        String fruits[] = {"apple", "mango", "orange"};
+    }
+}
